5982283b9b596ae705f021c37a69463d0a22590d
Truong Thien <caffeegg998@gmail.com>
2023-07-19 12:58:06 +0700

    done

--- react-test-app/src/components/Header/index.jsx
+++ react-test-app/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 
 const Index = () => {
-    return <h1>#1 Free Online Movie Streaming Site in Vietnam</h1>
+    return <h1 className='text-cyan-900'>#1 Free Online Movie Streaming Site in Vietnam</h1>
 };
 
 export default Index;
---
+++ react-test-app/src/components/Trending/movieData.js
@@ -0,0 +1,16 @@
+import  BatmanBegins from '../../assets/batman_begins.jpg'
+import  Dune from '../../assets/dune.jpg'
+import  Sing2 from '../../assets/sing_2.jpg'
+import  SpiderMAn from '../../assets/spider_man.jpg'
+import  TDK from '../../assets/the_dark_knight.jpg'
+import  TDKRises from '../../assets/the_dark_knight_rises.jpg'
+
+
+export const movieData = [
+    {src: BatmanBegins, title: 'Batman Begins', main: 'Chistian Bale', runtime: '120m'},
+    {src: Dune, title: 'Dune', main: 'Timothée Chalamet', runtime: '120m'},
+    {src: Sing2, title: 'Sing 2', main: 'Matthew MacConaughey', runtime: '120m'},
+    {src: SpiderMAn, title: 'Spider-man No Way Home', main: 'Tom Holland', runtime: '120m'},
+    {src: TDK, title: 'The Dark Knight', main: 'Chistian Bale', runtime: '120m'},
+    {src: TDKRises, title: 'The Dark Knight Rises', main: 'Chistian Bale', runtime: '120m'}
+]
