77df0270ae28b40950abe5038c9826999db0c8e0
Truong Thien <caffeegg998@gmail.com>
2023-07-22 10:18:52 +0700

    22 72

--- react-test-app/src/components/Nav/NavItem.jsx
+++ react-test-app/src/components/Nav/NavItem.jsx
@@ -13,6 +13,10 @@ const NavItem = ({ item }) => {
             <h3 className='mr-2'>{label}</h3>
             {icon}
         </li>
+    <li className={`flex p-2 justify-end items-center cursor-pointer ${active ? ' bg-primary text-white'  : ''}`}>
+        <h3 className='mr-2'>{label}</h3>
+        {icon}
+    </li>
     );
 };
 
